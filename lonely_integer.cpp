58bc7eb95c9b8a9b3aee319fa5f12e9ba5f6c065
Nabeel Khawar <leonabrocks@yahoo.com>
2016-09-14 23:33:15 +0530

    works

--- lonely_integer.cpp
+++ lonely_integer.cpp
@@ -5,19 +5,10 @@ typedef vector<int> vi;
 
 int findl(vi a, int n)
 {
-    int ans=-1;
+    int ans=0;
     for(int i=0;i<n;i++)
     {
-        for(int j=i+1;j<n-1;j++)
-        {
-            if((a[i]^a[j])==0)
-            {
-                ans=a[j];
-                break;
-            }
-            //else
-
-        }
+        ans^=a[i];
     }
     return ans;
 }
