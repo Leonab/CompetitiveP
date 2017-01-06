a29268465fe013c3b5b894a6d674b831fd4a2673
Nabeel Khawar <leonabrocks@yahoo.com>
2017-01-06 23:30:56 +0530

    sherlock-ed

--- dp/equal.cpp
+++ dp/equal.cpp
@@ -1,43 +1,51 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-#define rep(i,N) for(int (i)=0;(i)<(N);(i)++)
-#define repi(i,j,N) for(int (i)=(j);(i)<(N);(i)++)
-#define sz(a) int((a).size())
-#define pb push_back
-#define mp make_pair
-
-typedef vector<int> vi;
-typedef vector<vi> vvi;
-typedef pair<int,int> ii;
-
-int n,a[10000];
-int j=1,ways=0;
-
-int findOps( int minny, int index ) {
-    if( index < minny )
-        return 0;
-    if( index == minny ) {
-        j++;
-        return 1;
+long long int functn (long long int temp)
+{
+    long long int x=0;
+    if(temp>=5)
+    {
+        x = temp/5;
+        temp = temp%5;
     }
-    else
-        ways = min( findOps( minny, a[j]-1 ), min( findOps( minny, a[j]-2 ), findOps( minny, a[j]-5 ) ) );
+    if(temp>=2)
+    {
+        x += temp/2;
+        temp = temp%2;
+    }
+    x += temp;
+    return x;
 
-    return ways+1;
 }
 
 int main()
 {
-    int t;cin>>t;
-    while( t-- ){
-        cin>>n;
-        rep(i,n){
-            cin>>a[i];
+    long long int T,N,i,j,min,sum,temp;
+    cin>>T;
+    while(T--)
+    {
+        min = 1000000;
+        cin>>N;
+        int A[N];
+        for(i=0 ; i< N ; i++)
+        {
+            cin>>A[i];
+            if(A[i]<min)
+                min = A[i];
         }
-        sort(a,a+n);
-        int minny = a[0];
-        cout<<findOps( minny, a[j] );
+        long long int sum[6];
+        for(j=0 ; j<=5 ; j++)
+        {
+            sum[j]=0;
+            for(i=0 ; i< N ; i++)
+            {
+                temp = abs(A[i] - (min-j));
+                sum[j] = sum[j] + functn(temp);
+            }
+        }
+        sort(sum,sum+6);
+        cout<<sum[0]<<endl;
     }
     return 0;
 }
