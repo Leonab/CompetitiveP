38eeb841ae8fc67df928cfb94857f4b33019e3de
Nabeel Khawar <leonabrocks@yahoo.com>
2017-01-16 23:42:04 +0530

    stock max

---
+++ dp/stock_maximaize.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define rep(i,N) for(int (i)=0;(i)<(N);(i)++)
+#define repi(i,j,N) for(int (i)=(j);(i)<(N);(i)++)
+#define sz(a) int((a).size())
+#define pb push_back
+#define mp make_pair
+#define ll long long
+
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef pair<int,int> ii;
+
+int main()
+{
+    int t;cin>>t;
+    while(t--) {
+        ll n;cin>>n;
+        ll a[n];
+        for(ll i=0;i<n;i++) {
+            cin>>a[i];
+        }
+
+    }
+    return 0;
+}
