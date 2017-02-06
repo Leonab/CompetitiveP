5a76540fb1886411f45ccc5d486bfa8ba211344a
Nabeel Khawar <leonabrocks@yahoo.com>
2017-02-06 23:45:00 +0530

    bas ki nai hai

--- booking_couple_passions.cpp
+++ booking_couple_passions.cpp
@@ -14,5 +14,47 @@ typedef pair<int,int> ii;
 
 int main()
 {
+    int n,m;cin>>n;
+    string pass[256][256];
+    map<string,int> mp;
+    rep(i,n) {
+        cin>>m;
+        rep(j,m) {
+            cin>>pass[i][j];
+            mp[pass[i][j]] = 1;
+        }
+    }
+    int y;cin>>y;
+    multiset<pair<int,string> > storage;
+    rep(i,y) {
+        string dest="";
+        cin>>dest;
+        float lat,lng;cin>>lat>>lng;
+        int z;cin>>z;
+        int counter = 0;
+        rep(j,z) {
+            string pass2="";
+            cin>>pass2;
+
+            rep(l,n) {
+            rep(k,m) {
+                if(mp[pass[l][k]]==1)
+                if(pass2==pass[l][k])
+                    counter++,mp[pass[l][k]] += 1;
+                    //cout<<pass[l][k]<<' ';
+
+            }
+            }
+        }
+        storage.insert({counter,dest});
+    }
+    for(auto it:storage) {
+            cout<<"{"<<it.first<<","<<it.second<<"} ";
+    }cout<<endl;
+    int k = 0;
+    set<string> setty;
+    for(auto it:storage) {
+        cout<<it.second<<' ';
+    }
     return 0;
 }
