7b6a33e5bb5ead32f0ba6f4fb4770260b8a0c58f
Nabeel Khawar <leonabrocks@yahoo.com>
2017-02-21 22:49:40 +0530

    koi toh bacha lo

--- game_of_two_stacks.cpp
+++ game_of_two_stacks.cpp
@@ -18,29 +18,55 @@ int main(){
     for(int a0 = 0; a0 < g; a0++){
         ll n,m,x;
         cin >> n >> m >> x;
-        vector<ll> a(n);
+        vector<ll> a(100000);
         for(ll a_i = 0; a_i < n; a_i++){
            cin >> a[a_i];
         }
-        vector<ll> b(m);
+        vector<ll> b(100000);
         for(ll b_i = 0; b_i < m; b_i++){
            cin >> b[b_i];
         }
 
-        ll cnt = 0,sum = 0;
-        ll i=0,j=0;
-        while(sum + a[i]<=x || sum + b[j]<=x) {
-            if(a[i]<=b[j]) {
-                sum += a[i];
-                i++;
-            }
-            else {
-                sum += b[j];
-                j++;
-            }
-            cnt++;
+        ll suma[100000],sumb[100000];
+        suma[0] = a[0],sumb[0]=b[0];
+        for(ll i=1, j=1;i<n,j<m;i++,j++) {
+            suma[i] = suma[i-1] + a[i];
+            sumb[i] = sumb[i-1] + b[j];
         }
-        cout<<cnt<<endl;
+        ll ans,mx=0;
+        for(ll i=0;i<n;i++) {
+            ll zum = x - suma[i];
+            cout<<"zum="<<zum<<endl;
+            ll ans2 = 0;
+            if(zum>0) {
+            /*ll beg = 0, en = m-1;
+            while(beg<en) {
+                ll mid = (beg+en)/2;
+                if(sumb[mid]<=zum) {
+                    //cout<<sumb[mid]<<' '<<endl;
+                    ans2 = mid;
+                    en = mid-1;
+                }
+                else
+                    beg = mid+1;
+            }*/
+            for(ll j=0;j<m;j++) {
+                if(sumb[j]<=zum) {
+                    ans2=j;
+                    //break;
+                }
+                else
+                    break;
+            }
+            }
+            else
+                continue;
+            ans = i + ans2 + 2;
+            cout<<ans2<<' '<<i<<endl;
+            if(ans>mx)
+                mx = ans;
+        }
+        cout<<mx<<endl;
     }
     return 0;
 }
