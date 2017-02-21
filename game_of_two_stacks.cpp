552450e1596dbef3f4286f21efe98beb1948bc27
Nabeel Khawar <leonabrocks@yahoo.com>
2017-02-21 23:08:25 +0530

    no scene

--- game_of_two_stacks.cpp
+++ game_of_two_stacks.cpp
@@ -28,18 +28,18 @@ int main(){
         }
 
         ll suma[100000],sumb[100000];
-        suma[0] = a[0],sumb[0]=b[0];
-        for(ll i=1, j=1;i<n,j<m;i++,j++) {
+        suma[0] = a[0], sumb[0] = b[0];
+        for(ll i=1;i<n;i++)
             suma[i] = suma[i-1] + a[i];
-            sumb[i] = sumb[i-1] + b[j];
-        }
+        for(ll j=1;j<m;j++)
+            sumb[j] = sumb[j-1] + b[j];
         ll ans,mx=0;
+
         for(ll i=0;i<n;i++) {
             ll zum = x - suma[i];
-            cout<<"zum="<<zum<<endl;
             ll ans2 = 0;
             if(zum>0) {
-            /*ll beg = 0, en = m-1;
+            ll beg = 0, en = m-1;
             while(beg<en) {
                 ll mid = (beg+en)/2;
                 if(sumb[mid]<=zum) {
@@ -49,20 +49,19 @@ int main(){
                 }
                 else
                     beg = mid+1;
-            }*/
-            for(ll j=0;j<m;j++) {
+            }
+            /*for(ll j=0;j<m;j++) {
                 if(sumb[j]<=zum) {
                     ans2=j;
-                    //break;
                 }
                 else
                     break;
-            }
+            }*/
             }
             else
                 continue;
             ans = i + ans2 + 2;
-            cout<<ans2<<' '<<i<<endl;
+            //cout<<ans2<<' '<<i<<endl;
             if(ans>mx)
                 mx = ans;
         }
